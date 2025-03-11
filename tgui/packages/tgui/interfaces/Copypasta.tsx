dffc3668c5f59f8ca056a5385bae77e346138c64
Ez-Briz <nikita.gurko.2002@mail.ru>
2025-03-11 16:37:29 +0100

    Moduled copypasta interface

---
+++ tgui/packages/tgui/interfaces/Copypasta.tsx
@@ -0,0 +1,50 @@
+import { Button, Stack } from 'tgui-core/components';
+
+import { useBackend } from '../backend';
+import { Window } from '../layouts';
+
+export function Copypasta(props) {
+  const { act } = useBackend();
+  return (
+    <Window height={240} title="Game Panel" width={280} theme="admin">
+      <Window.Content>
+        <Stack
+          height="100%"
+          vertical
+          align="center"
+          verticalAlign="center"
+          textAlign="center"
+          direction="column"
+          justify="space-around"
+          fillPositionedParent
+        >
+          <Button
+            fluid
+            content="(Game Mode Panel)"
+            onClick={() => act('game-mode-panel')}
+          />
+          <Button
+            fluid
+            content="Create Object"
+            onClick={() => act('create-object')}
+          />
+          <Button
+            fluid
+            content="Quick Create Object"
+            onClick={() => act('quick-create-object')}
+          />
+          <Button
+            fluid
+            content="Create Turf"
+            onClick={() => act('create-turf')}
+          />
+          <Button
+            fluid
+            content="Create Mob"
+            onClick={() => act('create-mob')}
+          />
+        </Stack>
+      </Window.Content>
+    </Window>
+  );
+}
