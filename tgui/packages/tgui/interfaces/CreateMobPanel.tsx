fbef48e1209abf3fa3c5dd1acb8513773b7d3109
Ez-Briz <nikita.gurko.2002@mail.ru>
2025-03-18 16:08:48 +0100

    Copypaste interface but with title and broken list

---
+++ tgui/packages/tgui/interfaces/CreateMobPanel.tsx
@@ -0,0 +1,36 @@
+import { LabeledList, Stack } from 'tgui-core/components';
+
+import { useBackend } from '../backend';
+import { Window } from '../layouts';
+
+export function CreateMobPanel(props) {
+  const { act, data } = useBackend<Data>();
+  const { title, mobs } = data;
+  const listItems = mobs.map((item) => '<Table.Row>{item}</Table.Row>');
+  return (
+    <Window height={240} title={title} width={280} theme="admin">
+      <Window.Content>
+        <Stack
+          height="100%"
+          vertical
+          align="center"
+          verticalAlign="center"
+          textAlign="center"
+          direction="column"
+          justify="space-around"
+          fillPositionedParent
+        />
+        <LabeledList>
+          {mobs.map((mob) => {
+            return <LabeledList.Item key={mob}>{mob}</LabeledList.Item>;
+          })}
+        </LabeledList>
+      </Window.Content>
+    </Window>
+  );
+}
+
+export type Data = {
+  title: string;
+  mobs: string[];
+};
