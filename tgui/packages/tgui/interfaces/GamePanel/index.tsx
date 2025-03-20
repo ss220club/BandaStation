8a8eb44f77eb7e9b9ab39c1ee8bf6a60d3275ddd
Ez-Briz <nikita.gurko.2002@mail.ru>
2025-03-20 15:55:25 +0100

    almost done with create panels template

--- tgui/packages/tgui/interfaces/GamePanel/index.tsx
+++ tgui/packages/tgui/interfaces/GamePanel/index.tsx
@@ -1,13 +1,23 @@
 import { useState } from 'react';
-import { Button, Stack, Tabs } from 'tgui-core/components';
+import { Button, Dropdown, Input, Stack, Tabs } from 'tgui-core/components';
 
 import { useBackend } from '../../backend';
 import { Window } from '../../layouts';
 
 export function GamePanel(props) {
   const { act, data } = useBackend<GamePanelData>();
-  const { subwindowTitle, objList } = data;
+  const { subwindowTitle, objList, whereDropdownValue } = data;
   const [selectedTab, setSelectedTab] = useState(0);
+  const [selectedRadio, setSelectedRadio] = useState(1);
+  const [whereDropdownVal, setWhereDropdownVal] = useState(
+    'On floor below own mob',
+  );
+  const whereDropdownOptions = [
+    'On floor below own mob',
+    'On floor below own mob, dropped via supply pod',
+    "In own's mob hand",
+    'In marked object',
+  ];
   const tabs = [
     {
       content: 'Create Object',
@@ -82,8 +92,57 @@ export function GamePanel(props) {
           {/* Main window */}
           <Stack.Item grow basis="85%">
             {/* <CreateMobPanel mobs /> */}
-            {subwindowTitle}
-            {objList}
+            <Stack fill vertical>
+              <Stack.Item>
+                Type:
+                <Input
+                  width="280px"
+                  ml={1}
+                  placeholder={'Search for ' + subwindowTitle?.split(' ')[1]}
+                />
+                <Button onClick={() => act('update-search')}>Search</Button>
+              </Stack.Item>
+              <Stack.Item>
+                Offset: <Input width="250px" mr={2} placeholder="x,y,z" />
+                <Button.Checkbox
+                  circular
+                  selected={selectedRadio === 0}
+                  mr={1}
+                  icon="fa-a"
+                  onClick={() => {
+                    setSelectedRadio(0);
+                    act('set-absolute-cords');
+                  }}
+                />
+                <Button.Checkbox
+                  circular
+                  selected={selectedRadio === 1}
+                  icon="fa-r"
+                  onClick={() => {
+                    setSelectedRadio(1);
+                    act('set-relative-cords');
+                  }}
+                />
+              </Stack.Item>
+              <Stack.Item>
+                Number: <Input width="30px" mr={1} value={1} />
+                Dir: <Input width="30px" mr={1} />
+                Name: <Input width="180px" mr={1} />
+              </Stack.Item>
+              <Stack.Item>
+                <Dropdown
+                  width="320px"
+                  options={whereDropdownOptions}
+                  onSelected={(value) => {
+                    setWhereDropdownVal(value);
+                    act('where-dropdown-changed', {
+                      newWhere: value,
+                    });
+                  }}
+                  selected={whereDropdownVal}
+                />
+              </Stack.Item>
+            </Stack>
           </Stack.Item>
         </Stack>
       </Window.Content>
@@ -100,4 +159,5 @@ interface tab {
 export type GamePanelData = {
   subwindowTitle: string;
   objList: string[];
+  whereDropdownValue: string;
 };
