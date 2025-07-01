6cdcd36a170d4a25525e12911efbeab097ea7a3a
Aylong <alexanderkitsa@gmail.com>
2025-07-01 11:10:02 +0300

    Pon

--- tgui/packages/tgui/interfaces/Who/Ping.tsx
+++ tgui/packages/tgui/interfaces/Who/Ping.tsx
@@ -1,22 +1,22 @@
 import { Icon, Stack, Tooltip } from 'tgui-core/components';
 
 export function ShowPing(props) {
-  const { ping } = props.user;
-  const { lastPing, avgPing } = ping;
+  const { user } = props;
+  const ping = user?.ping;
 
   return (
     <Stack wrap>
       <Tooltip content="Текущий пинг" position="bottom-end">
         <Stack.Item color="green">
           <Icon mr={1} name="clock-rotate-left" />
-          {ping ? `${Math.round(lastPing)}ms` : <Icon name="infinity" />}
+          {ping ? `${Math.round(ping.lastPing)}ms` : <Icon name="infinity" />}
         </Stack.Item>
       </Tooltip>
       <Stack.Divider />
       <Tooltip content="Средний пинг" position="bottom-end">
         <Stack.Item color="orange">
           <Icon mr={1} name="chart-simple" />
-          {ping ? `${Math.round(avgPing)}ms` : <Icon name="infinity" />}
+          {ping ? `${Math.round(ping.avgPing)}ms` : <Icon name="infinity" />}
         </Stack.Item>
       </Tooltip>
     </Stack>
