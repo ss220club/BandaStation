aefd8e703be140b2a2d1d37c24b31b443c254beb
Aylong <69762909+AyIong@users.noreply.github.com>
2025-07-01 14:33:13 +0300

    Fix `Who` BSOD (#1541)
    ## Что этот PR делает
    Должно пофиксить БСОД когда чел не в своём теле и его чекают через WHO
    Fixes #1537
    
    Ну и пара дИзАйНерСкИх правок
    
    ## Почему это хорошо для игры
    Меньше багов
    
    ## Изображения изменений
    
    
    ![IrR2zvkVZf](https://github.com/user-attachments/assets/210acdfb-a217-485d-bca6-66f8741f6611)
    
    ## Тестирование
    Ну типа?
    
    ## Changelog
    
    :cl:
    fix: WHO больше не должен выдавать БЛЯТЬ когда вы чекаете битраннера в
    битране.
    /:cl:

--- tgui/packages/tgui/interfaces/Who/Ping.tsx
+++ tgui/packages/tgui/interfaces/Who/Ping.tsx
@@ -2,17 +2,21 @@ import { Icon, Stack, Tooltip } from 'tgui-core/components';
 
 export function ShowPing(props) {
   const { user } = props;
+  const ping = user?.ping;
+
   return (
     <Stack wrap>
-      <Tooltip content="Текущий пинг" position={'bottom-end'}>
+      <Tooltip content="Текущий пинг" position="bottom-end">
         <Stack.Item color="green">
-          <Icon name="clock-rotate-left" /> {Math.round(user.ping.lastPing)}ms
+          <Icon mr={1} name="clock-rotate-left" />
+          {ping ? `${Math.round(ping.lastPing)}ms` : <Icon name="infinity" />}
         </Stack.Item>
       </Tooltip>
       <Stack.Divider />
-      <Tooltip content="Средний пинг" position={'bottom-end'}>
+      <Tooltip content="Средний пинг" position="bottom-end">
         <Stack.Item color="orange">
-          <Icon name="chart-simple" /> {Math.round(user.ping.avgPing)}ms
+          <Icon mr={1} name="chart-simple" />
+          {ping ? `${Math.round(ping.avgPing)}ms` : <Icon name="infinity" />}
         </Stack.Item>
       </Tooltip>
     </Stack>
