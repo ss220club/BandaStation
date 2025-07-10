a6d8ffb98e114b0b4864f6b4ab8b0f7265d2f9e7
Aylong <alexanderkitsa@gmail.com>
2025-07-11 00:29:59 +0300

    Crafting fixes

--- tgui/packages/tgui/interfaces/PersonalCrafting/content/RecipeContent.tsx
+++ tgui/packages/tgui/interfaces/PersonalCrafting/content/RecipeContent.tsx
@@ -96,7 +96,7 @@ export function RecipeContentCompact(props: Props) {
                 <Box>
                   {!!item.tool_behaviors && (
                     <Tooltip
-                      content={'Tools: ' + item.tool_behaviors.join(', ')}
+                      content={'Инструменты: ' + item.tool_behaviors.join(', ')}
                     >
                       <Icon p={1} name="screwdriver-wrench" />
                     </Tooltip>
@@ -120,7 +120,7 @@ export function RecipeContentCompact(props: Props) {
                       })
                     }
                   >
-                    Make
+                    Создать
                   </Button>
                   {!!item.mass_craftable && (
                     <Button
@@ -128,7 +128,7 @@ export function RecipeContentCompact(props: Props) {
                       lineHeight={2.5}
                       width="32px"
                       align="center"
-                      tooltip="Repeat this craft until you run out of ingredients."
+                      tooltip="Продолжать создание, пока не закончатся ингредиенты."
                       tooltipPosition="top"
                       disabled={!craftable || busy}
                       icon="repeat"
@@ -292,7 +292,7 @@ export function RecipeContent(props: FullProps) {
                             })
                           }
                         >
-                          Make
+                          Создать
                         </Button>
                       </Stack.Item>
                       <Stack.Item>
@@ -320,7 +320,7 @@ export function RecipeContent(props: FullProps) {
                 <Stack.Item>
                   {!!item.complexity && (
                     <Box color="gray" width="104px" lineHeight={1.5} mt={1}>
-                      Complexity: {item.complexity}
+                      Сложность: {item.complexity}
                     </Box>
                   )}
                   {item.foodtypes?.length > 0 && (
