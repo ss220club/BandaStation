355b9a6422eaa9026ea7f88792836f6a46713809
Aylong <alexanderkitsa@gmail.com>
2025-07-12 01:24:33 +0300

    Wtf

--- tools/ci/check_misc.sh
+++ tools/ci/check_misc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -euo pipefail
 
-find . -name "*.json" -not -path "*/node_modules/*" -print0 | xargs -0 python3 ./tools/json_verifier.py
+find . -name "*.json" -not -path "*/node_modules/*" -not -path "*/_work/*" -print0 | xargs -0 python3 ./tools/json_verifier.py
