38f30b2145d478c026a2f26accfa8f8dd79a6a0d
Aylong <alexanderkitsa@gmail.com>
2025-07-12 21:33:02 +0300

    Whoopsie

--- tgui/packages/tgui/interfaces/PreferencesMenu/CharacterPreferences/MainPage.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/CharacterPreferences/MainPage.tsx
@@ -110,8 +110,8 @@ function GenderButton(props: GenderButtonProps) {
                 <Button
                   key={gender}
                   selected={gender === props.gender}
-                  icon={GENDERS[gender].icon || 'question'}
-                  tooltip={GENDERS[gender].text || 'Кто ты, воин?'}
+                  icon={GENDERS[gender]?.icon || 'question'}
+                  tooltip={GENDERS[gender]?.text || 'Кто ты, воин?'}
                   tooltipPosition="top"
                   onClick={() => {
                     props.handleSetGender(gender);
@@ -125,7 +125,7 @@ function GenderButton(props: GenderButtonProps) {
     >
       <div>
         <Button
-          icon={GENDERS[props.gender].icon || 'question'}
+          icon={GENDERS[props.gender]?.icon || 'question'}
           tooltip="Пол"
           tooltipPosition="top"
         />
