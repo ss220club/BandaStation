b5f213c1782daf94236754cacba36856f59d41b8
Voyaker-coder <sorokin.senya@yandex.ru>
2026-08-04 00:59:03 +0300

    added

---
+++ modular_bandastation/emote_wheel/_emote_wheel.dm
@@ -0,0 +1,16 @@
+/datum/modpack/emote_wheel
+	/// A string name for the modpack. Used for looking up other modpacks in init.
+	name = "emote_wheel"
+	/// A string desc for the modpack. Can be used for modpack verb list as description.
+	desc = "its useless"
+	/// A string with authors of this modpack.
+	author = "user"
+
+/datum/modpack/emote_wheel/pre_initialize()
+	. = ..()
+
+/datum/modpack/emote_wheel/initialize()
+	. = ..()
+
+/datum/modpack/emote_wheel/post_initialize()
+	. = ..()
---
+++ tgui/packages/tgui/interfaces/EmotePicker.tsx
@@ -0,0 +1,67 @@
+import { useBackend } from '../backend';
+import { Button, Input, Section, Stack } from 'tgui-core/components';
+import { Window } from '../layouts';
+import { useState } from 'react';
+
+type Emote = {
+  key: string;
+  name: string;
+  category: string;
+};
+
+type Data = {
+  slot: number;
+  emotes: Emote[];
+};
+
+export const EmotePicker = (_props, context) => {
+  const { act, data } = useBackend<Data>(context);
+
+  const [search, setSearch] = useState('');
+
+  const filtered = data.emotes.filter(e =>
+    e.name.toLowerCase().includes(search.toLowerCase()) ||
+    e.key.toLowerCase().includes(search.toLowerCase())
+  );
+
+  return (
+    <Window
+      width={420}
+      height={600}>
+      <Window.Content scrollable>
+        <Stack vertical fill>
+
+          <Stack.Item>
+            <Input
+              fluid
+              placeholder="Поиск..."
+              value={search}
+              onInput={(e, value) => setSearch(value)}
+            />
+          </Stack.Item>
+
+          <Stack.Item grow>
+            <Section title={`Эмоция для слота ${data.slot}`}>
+              {filtered.map(emote => (
+                <Button
+                  fluid
+                  key={emote.key}
+                  mb={0.5}
+                  onClick={() =>
+                    act('select', {
+                      key: emote.key,
+                    })
+                  }>
+                  {emote.name}
+                </Button>
+              ))}
+            </Section>
+          </Stack.Item>
+
+        </Stack>
+      </Window.Content>
+    </Window>
+  );
+};
+
+export default EmotePicker;
